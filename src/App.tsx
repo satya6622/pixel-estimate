d284a9b161aeb37d38142ef2ac6846728cd83ee8
veesamsatyateja <veesam.satyateja@bijak.in>
2025-10-14 22:26:35 +0530

    commit changes 5

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/create/:type" element={<CreateDocument />} />
+          {/* <Route path="/create/:type" element={<CreateDocument />} /> */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
