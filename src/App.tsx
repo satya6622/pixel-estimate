99e2a39e3f492e3b0d36cbc0fb3aaa9ed1cfdb3e
veesamsatyateja <veesam.satyateja@bijak.in>
2025-10-14 23:43:03 +0530

    commit changes 10

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ const App = () => (
       <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/create/:type" element={<CreateDocument />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
